0c2264ed928081ae104d3c462f460dab44793128
HARIRAK SRITHAM <harirak@hotmail.co.th>
2018-05-29 19:10:00 +0700

    Create new class instead of template class (program.cs)

--- Chapter07/Person.cs
+++ Chapter07/Person.cs
@@ -6,5 +6,26 @@ namespace Chapter07
 {
     class Person
     {
+        public string name, surname;
+        public bool gender;
+        public int age;
+        public string nationality;
+
+        public void Walk()
+        {
+            Console.WriteLine("Walk...");
+        }
+        
+        public void Run()
+        {
+            Console.WriteLine("Run...");
+        }
+
+        public void Talk(string sentence)
+        {
+            Console.WriteLine($"Sentence..."+sentence);
+        }
+
+
     }
 }
---
+++ Chapter07/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Chapter07
+{
+    class Starter
+    {
+        public static void Main(string[] args)
+        {
+            Person me = new Person();
+            me.name = "toon";
+            Console.WriteLine(me.name);
+        }
+         
+        
+    }
+}
