b4a23453229bccb4884fb353f68f69c9526b272c
HARIRAK SRITHAM <harirak@hotmail.co.th>
2018-05-29 18:31:22 +0700

    Add Person class

---
+++ Chapter07/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter07
+{
+    class Person
+    {
+    }
+}
---
+++ Chapter08/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+
+namespace Chapter08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+          
+           
+        }
+    }
+}
