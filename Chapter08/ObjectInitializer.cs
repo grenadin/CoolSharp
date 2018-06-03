481cec69823799ac791c53c533d589d944a1ef7a
HARIRAK SRITHAM <harirak@hotmail.co.th>
2018-06-04 00:37:00 +0700

    Finish OI,Properties

--- Chapter08/ObjectInitializer.cs
+++ Chapter08/ObjectInitializer.cs
@@ -6,19 +6,17 @@ namespace Chapter08
 {
     class ObjectInitializer
     {
-        
         string home_number;
         string building;
-
-        public ObjectInitializer(string home_number, string building)
-        {
-            this.Home_number = home_number;
-            this.Building = building;
-            Console.WriteLine(this.Home_number);
-        }
 
         public string Home_number { get => home_number; set => home_number = value; }
 
         public string Building { get => building; set => building = value; }
+
+        public ObjectInitializer()
+        {
+            home_number = "ctor1111";
+            Console.WriteLine(home_number);
+        }
     }
 }
--- Chapter08/Program.cs
+++ Chapter08/Program.cs
@@ -6,22 +6,23 @@ namespace Chapter08
     {
         static void Main(string[] args)
         {
-            NokiaPhone myMobile = new NokiaPhone("8350", "silver", true);
+            //NokiaPhone myMobile = new NokiaPhone("8350", "silver", true);
 
-            myMobile.Mask = "gold"; // this line set new mask. *** !beware this!! when call propertie it take action automatically
+            //myMobile.Mask = "gold"; // this line set new mask. *** !beware this!! when call propertie it take action automatically
 
-            NokiaPhone yourMobile = new NokiaPhone("8800", "red");
-            yourMobile.Mask = "gold"; // this line set new mask. *** ! beware this!! when call propertie it take action automatically
-
-
-            ObjectInitializer ObI = new ObjectInitializer("12/12", "IAC");
-            ObI.Home_number = "1111";
-
-            Console.WriteLine(ObI.Home_number);
-            Console.ReadLine();
+            //NokiaPhone yourMobile = new NokiaPhone("8800", "red");
+            //yourMobile.Mask = "gold"; // this line set new mask. *** ! beware this!! when call propertie it take action automatically
 
 
+            //ObjectInitializer ObI = new ObjectInitializer {Home_number="12/12"};
+            //Console.WriteLine(ObI.Home_number);
+
+            //ObI.Home_number = "1111";
            
+            //Console.WriteLine(ObI.Home_number);
+
+            //Console.ReadLine();
+
 
 
 
