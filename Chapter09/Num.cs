fc8ecfab87aca23c6bd3dde1128862d5a8c4731d
HARIRAK SRITHAM <harirak@hotmail.co.th>
2018-06-05 23:51:13 +0700

    Understand OOP is type.

---
+++ Chapter09/Num.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter09
+{
+    class Num
+    {
+        int data;
+
+
+        public Num()
+        {
+
+        }
+        public Num(int data)
+        {
+            this.data = data;
+            
+        }
+
+        public int Data { get => data; set => data = value; }
+
+
+        public static Num FindMax(Num x, Num y)
+        {
+            if (x.Data > y.Data)
+                
+                return x;
+            else
+                return y;
+        }
+
+    }
+
+  
+}
---
+++ Chapter09/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Num a = new Num(7);
+            //Num b = new Num(5);
+            //Num c = new Num();
+            //c.Data = Num.FindMax(a,b).Data;
+            //Console.WriteLine(c.Data);
+
+            //Console.ReadLine();
+
+            Console.WriteLine(Num.FindMax(new Num(7), new Num(5)).Data);
+        }
+    }
+}
