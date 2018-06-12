55b952caaa5ec49b8654ba37d2b4cd23caf44820
HARIRAK SRITHAM <harirak@hotmail.co.th>
2018-06-12 15:41:06 +0700

    Waiting for add code to delegate branch

---
+++ Delegate/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    class Hello
+    {
+        
+        public void Hi(string s)
+        {
+            Console.WriteLine("Hi this sentence is come from delegate call");
+        }
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System
+
+namespace Delegate
+{
+    delegate void Mydel(string s);
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Mydel de = new Mydel();
+            
+
+        }
+
+        
+    }
+}
